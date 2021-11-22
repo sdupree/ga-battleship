a7bada7ba81ef9f392290553e04f57918db6eab4
Shane Dupree <shane.dupree@gmail.com>
2021-11-22 18:30:38 -0500

    Refactor state variables and implement most of render() function.

--- battleship.js
+++ battleship.js
@@ -20,7 +20,7 @@ const ships = {
 
 
 /*----- Variables -----*/
-let player1_board, player2_board, player1_ships, player2_ships, winner, boardPositions;
+let players, turn, winner, boardPositions;
 
 
 /*----- Cached Element References -----*/
@@ -36,19 +36,32 @@ function init() {
   // Populate "boardPositions" array.
   generateBoardPositions();
 
-  player1_board = [];
-  player2_board = [];
+  // How many players?
+  players = [];
+  for(let i = 0; i < 2; i++) {
+    players[i] = {};
+    players[i].board = [];
+    players[i].ships = [];
+    players[i].id = `p${i + 1}`;
+  }
 
-  // Instantiate game boards.
-  [player1_board, player2_board].forEach(function(which_board) {
+  // Initialilze each player's board and ships.
+  players.forEach(function(player) {
+    // Instantiate player's game board.
     boardPositions.forEach(function(pos) {
-      which_board[pos] = null;
+      player.board[pos] = null;
     });
+    // Initialize player's ships.
+    for(const ship in ships) {
+      player.ships[ship] = ships[ship];
+      player.ships[ship].isSunk = false;
+      player.ships[ship].topLeft = undefined;
+      player.ships[ship].orientation = undefined;
+    }
+
+    // Construct game board HTML.
+    document.querySelector(`#${player.id}-board`).innerHTML = generateBoardHTML(player.id);
   });
-  
-  // Construct game board HTML.
-  document.querySelector('#player1-board').innerHTML = generateBoardHTML('p1');
-  document.querySelector('#player2-board').innerHTML = generateBoardHTML('p2');
 
   // Make random pegs (optional)
   ['p1', 'p2'].forEach(function(which_board) {
@@ -58,15 +71,17 @@ function init() {
       if(Math.floor(Math.random() * 7) % 7 == 0) {
         if(Math.floor(Math.random() * 7) % 7 == 0) {
           span.classList.add('peg', 'red-peg');
-          // div.classList.remove('peg-hole');
+          span.classList.remove('peg-hole');
         } else {
           span.classList.add('peg', 'white-peg');
-          // div.classList.remove('peg-hole');
+          span.classList.remove('peg-hole');
         }
       }
     });
   });
 
+  // Whose turn is it?
+  turn = 'p1';
 
   // Initialize "winner".
   winner = undefined;
@@ -98,7 +113,6 @@ function generateBoardHTML(player) {
 
   // Generate middle 10 rows.
   boardPositions.forEach(function(pos, idx) {
-    console.log(pos);
     if(idx % 10 === 0) { boardHtml += '<div class="board-border-square">' + pos.charAt() + '</div>'; }
     boardHtml += `<div class="board-square" id="${player}-${pos}"><span class="peg-hole"></span></div>`;
     if(idx % 10 === 9) { boardHtml += '<div class="board-border-square"></div>\n'; }
@@ -114,34 +128,70 @@ function generateBoardHTML(player) {
 }
 
 function render() {
-  // First render board (hits/misses)
+  players.forEach(function(player) {
+    // First render board (hits/misses)
+    for(const boardSquareID in player.board) {
+      // Get HTML elements.
+      const boardSquare = document.getElementById(`${player.id}-${boardSquareID}`);
+      const boardSquareSpan = boardSquare.querySelector('span');
+
+      // Clear all non-default classes.
+      // Trying to loop over an array while you remove elements causes odd behavior, so copy the array first!
+      let classesToRemove = [];
+      classesToRemove = [...boardSquare.classList];
+      classesToRemove.forEach(function(htmlClass) {
+        if(htmlClass !== 'board-square') boardSquare.classList.remove(htmlClass);
+      });
+      classesToRemove = [...boardSquareSpan.classList];
+      classesToRemove.forEach(function(htmlClass, i) {
+        boardSquareSpan.classList.remove(htmlClass);
+      });
+
+      // Add base classes back in.
+      if(player.board[boardSquareID]) {
+        // Has been hit.
+        boardSquareSpan.classList.add('peg', 'white-peg');
+      } else {
+        // Has not been hit.
+        boardSquareSpan.classList.add('peg-hole');
+      }
+    }
+  });
+
   // Then render ships
+
+
   // Then render messages (or is messaging separate?)
+
+
 }
 
 function renderShip(ship, topLeft, whichBoard, borderColor) {
-  topLeft = 'A1';
+  topLeft = topLeft ? topLeft : 'A1';
+  playerBoard = 'p2';
+
   let targetGridSquare = topLeft;
   let shipLength = 3;
 
-  let targetDiv = document.getElementById(targetGridSquare);
+  let targetDiv = document.getElementById(`${playerBoard}-${targetGridSquare}`);
   targetDiv.classList.add('ship-left');
   targetGridSquare = gridAdd(targetGridSquare, 'col');
 
   for(i = 0; i < shipLength - 2; i++) {
-    let targetDiv = document.getElementById(targetGridSquare);
+    let targetDiv = document.getElementById(`${playerBoard}-${targetGridSquare}`);
     targetDiv.classList.add('ship-lr-mid');
     targetGridSquare = gridAdd(targetGridSquare, 'col');
   }
-  targetDiv = document.getElementById(targetGridSquare);
+  targetDiv = document.getElementById(`${playerBoard}-${targetGridSquare}`);
   targetDiv.classList.add('ship-right');
 
 }
 
-function gridAdd(pos, dir) {
-  // Add 1 position to the 'row' or 'column' of the grid square we were passed, and return it.
+function gridAdd(pos, dir, num) {
+  // Add 'num' position(s) [default 1] to the 'row' or 'column' of the grid square we were passed, and return it, or 'undefined' if out of bounds.
   let row = pos.substring(0, 1);
   let col = pos.substring(1);
+  num ? num : 1;
 
   if(dir === 'row') { row = String.fromCharCode(row.charCodeAt() + 1); }
   else if(dir === 'col') { col = parseInt(col) + 1; }
